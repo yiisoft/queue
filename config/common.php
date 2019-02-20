ab2fdea454443244afb97107ca2701025a814b82
Kids Return <leonsw.com@gmail.com>
2019-02-21 04:41:23 +0800

    Moved config from console to common

--- config/common.php
+++ config/common.php
@@ -6,4 +6,8 @@ return [
     'queue' => [
         '__class' => \yii\queue\sync\Queue::class,
     ],
+    \yii\queue\serializers\SerializerInterface::class => Reference::to('queue.serializer'),
+    'queue.serializer' => [
+        '__class' => \yii\queue\serializers\PhpSerializer::class,
+    ],
 ];
