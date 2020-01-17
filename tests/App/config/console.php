3b9aa3fabf0d03d7b0ab77e8c6796823e993765f
viktorprogger <viktorprogger@gmail.com>
2020-01-17 12:48:01 +0300

    Resolve dev dependencies

--- tests/App/config/console.php
+++ tests/App/config/console.php
@@ -1,10 +1,16 @@
 <?php
 
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\CommandLoader\ContainerCommandLoader;
+use Yiisoft\Db\Migration;
+use Yiisoft\Yii\Console\Application;
+use Yiisoft\Yii\Queue\Tests\App\Benchmark\Controller;
+
 return [
-    'app' => [
-        'controllerMap' => [
+    Application::class => static function (ContainerInterface $container) {
+        $commands = [
             'mysql-migrate' => [
-                '__class' => \Yiisoft\Yii\Console\Controllers\MigrateController::class,
+                '__class' => Migration::class,
                 'db' => 'mysql',
                 'migrationPath' => null,
                 'migrationNamespaces' => [
@@ -12,7 +18,7 @@ return [
                 ],
             ],
             'sqlite-migrate' => [
-                '__class' => \Yiisoft\Yii\Console\Controllers\MigrateController::class,
+                '__class' => Migration::class,
                 'db' => 'sqlite',
                 'migrationPath' => null,
                 'migrationNamespaces' => [
@@ -20,14 +26,23 @@ return [
                 ],
             ],
             'pgsql-migrate' => [
-                '__class' => \Yiisoft\Yii\Console\Controllers\MigrateController::class,
+                '__class' => Migration::class,
                 'db' => 'pgsql',
                 'migrationPath' => null,
                 'migrationNamespaces' => [
                     'Yiisoft\Yii\Queue\Drivers\Db\migrations',
                 ],
             ],
-            'benchmark' => \Yiisoft\Yii\Queue\Tests\App\Benchmark\Controller::class,
-        ],
-    ],
+            'benchmark' => Controller::class,
+        ];
+
+        $app = new Application();
+        $loader = new ContainerCommandLoader(
+            $container,
+            $commands
+        );
+        $app->setCommandLoader($loader);
+
+        return $app;
+    },
 ];
