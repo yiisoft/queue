2306a3d832e16c02d21f052cc55e463155c5eba6
Viktor <viktorprogger@gmail.com>
2020-03-23 17:23:40 +0300

    Fix #24: Add console commands (#36)

---
+++ src/Commands/RunCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\Queue\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Yiisoft\Yii\Queue\Queue;
+
+class RunCommand extends Command
+{
+    protected static $defaultName = 'queue/run';
+    private Queue $queue;
+
+    public function __construct(?string $name, Queue $queue)
+    {
+        parent::__construct($name);
+        $this->queue = $queue;
+
+        $this->setDescription('Runs all the existing messages in the queue. Exits once messages are over.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->queue->run();
+    }
+}
