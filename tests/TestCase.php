b4afdc63b4ef9c24ab66aaae09b5a046818a6f2c
viktorprogger <viktorprogger@gmail.com>
2020-09-11 18:51:24 +0300

    Dependency removing is started

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,11 +8,20 @@
 
 namespace Yiisoft\Yii\Queue\Tests;
 
-use Yiisoft\Composer\Config\Builder;
+use Psr\Container\ContainerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\Log\NullLogger;
 use PHPUnit\Framework\TestCase as BaseTestCase;
-use Yiisoft\Di\Container;
-use Yiisoft\Yii\Event\EventDispatcherProvider;
+use Yiisoft\Injector\Injector;
+use Yiisoft\Test\Support\Container\SimpleContainer;
+use Yiisoft\Test\Support\EventDispatcher\SimpleEventDispatcher;
+use Yiisoft\Yii\Queue\Cli\LoopInterface;
+use Yiisoft\Yii\Queue\Cli\SignalLoop;
+use Yiisoft\Yii\Queue\Driver\DriverInterface;
+use Yiisoft\Yii\Queue\Queue;
 use Yiisoft\Yii\Queue\Tests\App\ContainerConfigurator;
+use Yiisoft\Yii\Queue\Worker\Worker;
+use Yiisoft\Yii\Queue\Worker\WorkerInterface;
 
 /**
  * Base Test Case.
@@ -30,5 +39,57 @@ abstract class TestCase extends BaseTestCase
         $this->containerConfigurator = new ContainerConfigurator($this->container);
         $eventConfigurator = $this->container->get(EventDispatcherProvider::class);
         $eventConfigurator->register($this->container);
+    }
+
+    protected function getQueue(): Queue
+    {
+        return new Queue(
+            $this->getDriver(),
+            $this->getEventDispatcher(),
+            $this->getWorker(),
+            $this->getLoop(),
+            new NullLogger()
+        );
+    }
+
+    protected function getDriver(): DriverInterface
+    {
+        return $this->createMock(DriverInterface::class);
+    }
+
+    protected function getLoop(): LoopInterface
+    {
+        return new SignalLoop();
+    }
+
+    protected function getWorker(): WorkerInterface
+    {
+        return new Worker(
+            $this->getEventHandlers(),
+            $this->getEventDispatcher(),
+            new NullLogger(),
+            new Injector($this->getContainer()),
+            $this->getContainer()
+        );
+    }
+
+    protected function getEventDispatcher(): EventDispatcherInterface
+    {
+        return new SimpleEventDispatcher();
+    }
+
+    private function getEventHandlers(): array
+    {
+        return [];
+    }
+
+    protected function getContainer(): ContainerInterface
+    {
+        return new SimpleContainer($this->getContainerDefinitions());
+    }
+
+    protected function getContainerDefinitions(): array
+    {
+        return [];
     }
 }
