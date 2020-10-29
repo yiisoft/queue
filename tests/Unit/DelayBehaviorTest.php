03cfad2d77c1281d8c140754e99b90ebb85a3625
viktorprogger <viktorprogger@gmail.com>
2020-10-29 18:04:03 +0300

    Behavior tests

---
+++ tests/Unit/DelayBehaviorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\Queue\Tests\Unit;
+
+use Yiisoft\Yii\Queue\Message\Behaviors\DelayBehavior;
+use Yiisoft\Yii\Queue\Tests\TestCase;
+
+class DelayBehaviorTest extends TestCase
+{
+    public function testConstructorParameters(): void
+    {
+        $delay = 5;
+        $behavior = new DelayBehavior($delay);
+
+        self::assertEquals([$delay], $behavior->getConstructorParameters());
+    }
+
+    public function testConstructorParametersUsage(): void
+    {
+        $delay = 5;
+        $behavior = new DelayBehavior($delay);
+        $behavior2 = new DelayBehavior(...$behavior->getConstructorParameters());
+
+        self::assertEquals($behavior->getDelay(), $behavior2->getDelay());
+    }
+
+    public function testGetDelay(): void
+    {
+        $delay = 5;
+        $behavior = new DelayBehavior($delay);
+
+        self::assertEquals($delay, $behavior->getDelay());
+    }
+}
