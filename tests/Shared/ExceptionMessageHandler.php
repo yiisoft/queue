8f1bb1d07dd11888d691c394209b67a4dd1f3231
Dmitrii Derepko <xepozz@list.ru>
2024-02-15 00:50:01 +0700

    Share test stuff

---
+++ tests/Shared/ExceptionMessageHandler.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Queue\Tests\Shared;
+
+use RuntimeException;
+use Yiisoft\Queue\Message\MessageHandlerInterface;
+use Yiisoft\Queue\Message\MessageInterface;
+
+class ExceptionMessageHandler implements MessageHandlerInterface
+{
+    public function handle(MessageInterface $message): void
+    {
+        throw new RuntimeException('Test exception');
+    }
+}
