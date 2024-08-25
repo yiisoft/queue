22a0456d43b4d2d1a3c55ffe3afd04b666aa8b82
viktorprogger <viktorprogger@gmail.com>
2024-08-25 17:42:11 +0500

    A couple more assertions in tests

--- tests/Unit/EnvelopeTest.php
+++ tests/Unit/EnvelopeTest.php
@@ -24,24 +24,27 @@ final class EnvelopeTest extends TestCase
         $this->assertEquals([
             IdEnvelope::class,
         ], $stack);
+
+        $this->assertEquals('test-id', $message->getMetadata()[IdEnvelope::MESSAGE_ID_KEY]);
     }
 
     public function testEnvelopeDuplicates(): void
     {
         $message = new Message('handler', 'test');
-        $message = new IdEnvelope($message, 'test-id');
-        $message = new IdEnvelope($message, 'test-id');
-        $message = new IdEnvelope($message, 'test-id');
+        $message = new IdEnvelope($message, 'test-id-1');
+        $message = new IdEnvelope($message, 'test-id-2');
+        $message = new IdEnvelope($message, 'test-id-3');
 
         $this->assertEquals('test', $message->getMessage()->getData());
 
         $stack = $message->getMetadata()[EnvelopeInterface::ENVELOPE_STACK_KEY];
         $this->assertIsArray($stack);
-
         $this->assertEquals([
             IdEnvelope::class,
             IdEnvelope::class,
             IdEnvelope::class,
         ], $stack);
+
+        $this->assertEquals('test-id-3', $message->getMetadata()[IdEnvelope::MESSAGE_ID_KEY]);
     }
 }
