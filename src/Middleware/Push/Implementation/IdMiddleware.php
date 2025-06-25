a4b73cc997c53dfa5aa39e75fd70bb3a8da64fe9
viktorprogger <viktorprogger@gmail.com>
2025-06-25 19:25:18 +0500

    Add a middleware for automatic ID creation for messages before push

---
+++ src/Middleware/Push/Implementation/IdMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Queue\Middleware\Push\Implementation;
+
+use Yiisoft\Queue\Message\IdEnvelope;
+use Yiisoft\Queue\Middleware\Push\MiddlewarePushInterface;
+use Yiisoft\Queue\Middleware\Push\PushRequest;
+use Yiisoft\Queue\Middleware\Push\MessageHandlerPushInterface;
+
+/**
+ * A middleware interface for message ID setting. It must be implemented in an adapter package or in a project.
+ */
+final class IdMiddleware implements MiddlewarePushInterface
+{
+    public function processPush(PushRequest $request, MessageHandlerPushInterface $handler): PushRequest
+    {
+        if (($request->getMessage()->getMetadata()[IdEnvelope::MESSAGE_ID_KEY] ?? null) === null) {
+            $request = $request->withMessage(new IdEnvelope($request->getMessage(), uniqid('yii3-message-', true)));
+        }
+
+        return $handler->handlePush($request);
+    }
+}
