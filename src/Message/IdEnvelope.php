e00d295127c1a72cff448c8803994427adfaee2d
viktorprogger <viktorprogger@gmail.com>
2025-07-13 17:47:12 +0500

    Add envelope tests

--- src/Message/IdEnvelope.php
+++ src/Message/IdEnvelope.php
@@ -21,7 +21,7 @@ final class IdEnvelope implements EnvelopeInterface
 
     public static function fromMessage(MessageInterface $message): self
     {
-        return new self($message, $message->getMetadata()[self::MESSAGE_ID_KEY] ?? null);
+        return new self($message, self::getIdFromMessage($message));
     }
 
     public function setId(string|int|null $id): void
@@ -31,11 +31,27 @@ final class IdEnvelope implements EnvelopeInterface
 
     public function getId(): string|int|null
     {
-        return $this->id ?? $this->message->getMetadata()[self::MESSAGE_ID_KEY] ?? null;
+        return $this->id ?? self::getIdFromMessage($this->message);
     }
 
     private function getEnvelopeMetadata(): array
     {
         return [self::MESSAGE_ID_KEY => $this->getId()];
+    }
+
+    private static function getIdFromMessage(MessageInterface $message): string|int|null
+    {
+        $id = $message->getMetadata()[self::MESSAGE_ID_KEY] ?? null;
+        if ($id instanceof \Stringable) {
+            $id = (string) $id;
+        }
+
+        // We don't throw an error as this value could come from external sources,
+        // and we should process the message either way
+        if (!is_string($id) && !is_int($id)) {
+            return null;
+        }
+
+        return $id;
     }
 }
