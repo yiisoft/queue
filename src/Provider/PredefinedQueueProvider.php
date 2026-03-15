aed00ec2a49eb2608a9500d8f44f4a2eaf68df35
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-03-15 20:12:27 +0300

    Add `PredefinedQueueProvider`

---
+++ src/Provider/PredefinedQueueProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Queue\Provider;
+
+use BackedEnum;
+use Yiisoft\Queue\QueueInterface;
+use Yiisoft\Queue\StringNormalizer;
+
+use function array_key_exists;
+use function get_debug_type;
+use function sprintf;
+
+/**
+ * Queue provider that uses a pre-defined map of queue name to queue instance.
+ */
+final class PredefinedQueueProvider implements QueueProviderInterface
+{
+    /**
+     * @psalm-var array<string, QueueInterface>
+     */
+    private readonly array $queues;
+
+    /**
+     * @param array $queues Map of queue name to queue instance.
+     *
+     * @psalm-param array<string, QueueInterface> $queues
+     *
+     * @throws InvalidQueueConfigException If a value in the array is not a {@see QueueInterface} instance.
+     */
+    public function __construct(array $queues)
+    {
+        foreach ($queues as $name => $queue) {
+            if (!$queue instanceof QueueInterface) {
+                throw new InvalidQueueConfigException(
+                    sprintf(
+                        'Queue must implement "%s". For queue "%s" got "%s" instead.',
+                        QueueInterface::class,
+                        $name,
+                        get_debug_type($queue),
+                    ),
+                );
+            }
+        }
+        $this->queues = $queues;
+    }
+
+    public function get(string|BackedEnum $name): QueueInterface
+    {
+        $name = StringNormalizer::normalize($name);
+
+        if (!array_key_exists($name, $this->queues)) {
+            throw new QueueNotFoundException($name);
+        }
+
+        return $this->queues[$name];
+    }
+
+    public function has(string|BackedEnum $name): bool
+    {
+        $name = StringNormalizer::normalize($name);
+        return array_key_exists($name, $this->queues);
+    }
+}
---
+++ tests/Unit/Provider/PredefinedQueueProviderTest.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Queue\Tests\Unit\Provider;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Queue\Provider\InvalidQueueConfigException;
+use Yiisoft\Queue\Provider\QueueNotFoundException;
+use Yiisoft\Queue\Provider\PredefinedQueueProvider;
+use Yiisoft\Queue\QueueInterface;
+use Yiisoft\Queue\Stubs\StubQueue;
+use Yiisoft\Queue\Tests\Unit\Support\StringEnum;
+
+use function sprintf;
+
+final class PredefinedQueueProviderTest extends TestCase
+{
+    public function testBase(): void
+    {
+        $queue = new StubQueue();
+        $provider = new PredefinedQueueProvider([
+            'queue1' => $queue,
+        ]);
+
+        $this->assertSame($queue, $provider->get('queue1'));
+        $this->assertTrue($provider->has('queue1'));
+        $this->assertFalse($provider->has('not-exist-queue'));
+    }
+
+    public function testGetTwice(): void
+    {
+        $queue = new StubQueue();
+        $provider = new PredefinedQueueProvider([
+            'queue1' => $queue,
+        ]);
+
+        $queue1 = $provider->get('queue1');
+        $queue2 = $provider->get('queue1');
+
+        $this->assertSame($queue1, $queue2);
+    }
+
+    public function testGetNotExistQueue(): void
+    {
+        $provider = new PredefinedQueueProvider([
+            'queue1' => new StubQueue(),
+        ]);
+
+        $this->expectException(QueueNotFoundException::class);
+        $this->expectExceptionMessage('Queue with name "not-exist-queue" not found.');
+        $provider->get('not-exist-queue');
+    }
+
+    public function testInvalidQueueConfig(): void
+    {
+        $this->expectException(InvalidQueueConfigException::class);
+        $this->expectExceptionMessage(
+            sprintf(
+                'Queue must implement "%s". For queue "%s" got "%s" instead.',
+                QueueInterface::class,
+                'queue1',
+                'stdClass',
+            ),
+        );
+        new PredefinedQueueProvider([
+            'queue1' => new \stdClass(),
+        ]);
+    }
+
+    public function testGetHasByStringEnum(): void
+    {
+        $queue = new StubQueue();
+        $provider = new PredefinedQueueProvider([
+            'red' => $queue,
+        ]);
+
+        $this->assertSame($queue, $provider->get(StringEnum::RED));
+        $this->assertTrue($provider->has(StringEnum::RED));
+        $this->assertFalse($provider->has(StringEnum::GREEN));
+    }
+
+    public function testEmpty(): void
+    {
+        $provider = new PredefinedQueueProvider([]);
+
+        $this->assertFalse($provider->has('any'));
+    }
+}
