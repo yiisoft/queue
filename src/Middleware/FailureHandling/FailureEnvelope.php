d70014b3164bd27bce4560a172f1534bcffa4132
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-06-04 14:36:11 +0300

    Improve `FailureEnvelope`

--- src/Middleware/FailureHandling/FailureEnvelope.php
+++ src/Middleware/FailureHandling/FailureEnvelope.php
@@ -8,22 +8,51 @@ use Yiisoft\Arrays\ArrayHelper;
 use Yiisoft\Queue\Message\Envelope;
 use Yiisoft\Queue\Message\MessageInterface;
 
+use function array_key_exists;
+use function is_array;
+
 final class FailureEnvelope extends Envelope
 {
     public const META_FAILURE_METADATA = 'yii-failure-metadata';
 
+    private array $failureMetadata;
+
     public function __construct(MessageInterface $message, array $failureMetadata = [])
     {
+        $this->failureMetadata = ArrayHelper::merge(
+            self::getFailureMetadataFromMessage($message),
+            $failureMetadata,
+        );
         parent::__construct($message, [
-            self::META_FAILURE_METADATA => ArrayHelper::merge($message->getMetadata()[self::META_FAILURE_METADATA] ?? [], $failureMetadata),
+            self::META_FAILURE_METADATA => $this->failureMetadata,
         ]);
+    }
+
+    public function getFailureMetadata(): array
+    {
+        return $this->failureMetadata;
+    }
+
+    public function getFailureMetadataValue(string $key, mixed $default = null): mixed
+    {
+        return $this->failureMetadata[$key] ?? $default;
     }
 
     public static function fromMessage(MessageInterface $message): static
     {
-        /** @var array $metadata */
-        $metadata = $message->getMetadata()[self::META_FAILURE_METADATA] ?? [];
+        return new self(
+            $message,
+            self::getFailureMetadataFromMessage($message),
+        );
+    }
 
-        return new self($message, $metadata);
+    private static function getFailureMetadataFromMessage(MessageInterface $message): array
+    {
+        $metadata = $message->getMetadata();
+        if (array_key_exists(self::META_FAILURE_METADATA, $metadata)) {
+            $result = $metadata[self::META_FAILURE_METADATA];
+            return is_array($result) ? $result : [];
+        }
+        return [];
     }
 }
--- src/Middleware/FailureHandling/Implementation/ExponentialDelayMiddleware.php
+++ src/Middleware/FailureHandling/Implementation/ExponentialDelayMiddleware.php
@@ -87,18 +87,15 @@ final class ExponentialDelayMiddleware implements FailureMiddlewareInterface
 
     private function getAttempts(MessageInterface $message): int
     {
-        /** @var array{failure-strategy-exponential-delay-attempts?: int} $failureMeta */
-        $failureMeta = $message->getMetadata()[FailureEnvelope::META_FAILURE_METADATA] ?? [];
-        return $failureMeta[self::META_KEY_ATTEMPTS . "-$this->id"] ?? 0;
+        return (int) FailureEnvelope::fromMessage($message)
+            ->getFailureMetadataValue(self::META_KEY_ATTEMPTS . "-$this->id", 0);
     }
 
     private function getDelay(MessageInterface $message): float
     {
-        /** @var array{failure-strategy-exponential-delay-delay?: float} $meta */
-        $meta = $message->getMetadata()[FailureEnvelope::META_FAILURE_METADATA] ?? [];
-        $key = self::META_KEY_DELAY . "-$this->id";
+        $delayOriginal = (float) FailureEnvelope::fromMessage($message)
+            ->getFailureMetadataValue(self::META_KEY_DELAY . "-$this->id", 0);
 
-        $delayOriginal = (float) ($meta[$key] ?? 0);
         if ($delayOriginal <= 0) {
             $delayOriginal = $this->delayInitial;
         }
